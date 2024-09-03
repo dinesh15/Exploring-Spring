f8515c86eafe21c8ce3ca59ce90bf8301a8ddbda
Dinesh Raj <dineshpampati@gmail.com>
2024-09-02 20:33:28 -0500

    changing the folder directory

--- src/main/java/com/dinesh/springboot/myfirstwebapp/todo/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dinesh.springboot.myfirstwebapp.todo;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer>{
-    public List<Todo> findByuserName(String username);
-}
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
-VALUES(1001, 'dinesh', 'email to rec', CURRENT_DATE(), false);
-
-insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
-VALUES(1002, 'dinesh', 'email to rec', CURRENT_DATE, false);
-
-insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
-VALUES(1003, 'dinesh', 'email to rec', CURRENT_DATE, false);
-
