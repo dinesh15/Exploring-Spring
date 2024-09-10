68d7086a09f23a074e508685ffc63a25e455383d
Dinesh Raj <dineshpampati@gmail.com>
2024-09-10 18:17:37 -0500

    i18n

--- restfull-webservices/src/main/java/com/dinesh/rest/webservies/helloworld/HelloWordRESTController.java
+++ restfull-webservices/src/main/java/com/dinesh/rest/webservies/helloworld/HelloWordRESTController.java
@@ -1,17 +1,24 @@
 package com.dinesh.rest.webservies.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWordRESTController {
 
+    MessageSource messageSource;
+
     private HelloWorld helloWorld;
 
-    public HelloWordRESTController(HelloWorld helloWorld) {
+    public HelloWordRESTController(HelloWorld helloWorld, MessageSource messageSource) {
         this.helloWorld = helloWorld;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/helloworld")
@@ -19,9 +26,16 @@ public class HelloWordRESTController {
         return helloWorld;
     }
 
+
+
     @GetMapping("/helloworld/{message}")
     public HelloWorld getHelloWorld(@PathVariable String message) {
         return new HelloWorld(message);
+    }
+    @GetMapping("/helloworld-i18n")
+    public String getHelloWorldI18N() {
+        Locale Locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message", Locale);
     }
 
 }
